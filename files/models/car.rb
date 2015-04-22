04b04fecd9127707d856e6faba8ba1f1295aa617
Annie Pennell <annie@anniepennell.com>
2015-04-22 10:51:50 -0700

    created fast_car.rb

--- files/models/car.rb
+++ files/models/car.rb
@@ -1,11 +1,15 @@
+# create Car class
 class Car
+  # makes make and model attributes that can be read and written
   attr_accessor :make, :model
 
+  # initialize instance of car and gives it make and model attributes
   def initialize(make,model)
     @make = make
     @model = model
   end
 
+  # makes a speed attribute, which is set to the given speed
   def go_to_speed(speed)
     @speed = speed
   end
--- files/models/convertible.rb
+++ files/models/convertible.rb
@@ -1,21 +1,29 @@
+# links us to the car model so that we can use the attributes from that when creating a convertible instance
 require_relative 'car.rb'
 
+# create Convertible class
 class Convertible < Car
+  # makes color attribute with read only
   attr_reader :color
 
+  # initialize instance of convertible and give it make and model attributes from the super Car class, as well as the new color attribtue
   def initialize(make,model,color)
+    # takes make and model attributes from super Car model
     super(make,model)
     @color = color
     @open = false
   end
 
+  # toggles @open state 
   def convert
     @open = !@open
   end
 
   def go_to_speed(speed)
     puts "Top coming down"
+    # runs convert method to toggle top to open 
     convert
+    # Car's go_to_speed method
     super(speed)
   end
 end
